460efa6b44ea7a302e464d6e3dbbe5e675f27420
LRicciardo <liane.ricciardo@gmail.com>
2023-02-13 20:50:27 -0600

    refactor StoreContext to React-Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,10 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+// import { StoreProvider } from './utils/GlobalState';
+import { Provider } from "react-redux";
+import store from "./utils/store"
+
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
@@ -42,7 +45,8 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          {/* <StoreProvider> */}
+          <Provider store = { store } >
             <Nav />
             <Routes>
               <Route 
@@ -74,7 +78,8 @@ function App() {
                 element={<NoMatch />} 
               />
             </Routes>
-          </StoreProvider>
+          </Provider>
+          {/* </StoreProvider> */}
         </div>
       </Router>
     </ApolloProvider>
