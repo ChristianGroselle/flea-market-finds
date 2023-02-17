61068a33e2f7af841509b851908ab99668d5f483
Micky-Ad <110940136+Micky-Ad@users.noreply.github.com>
2023-02-17 17:13:11 -0500

    updates to stripe and env

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
@@ -30,6 +30,27 @@ export const ADD_ORDER = gql`
 `;
 
 export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_BOOTH = gql`
   mutation addUser(
     $firstName: String!
     $lastName: String!
